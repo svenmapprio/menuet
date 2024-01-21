07d9a344ef347a13632431bc7d81048bdf8b5d2b
svenfilify <sven@filify.app>
2024-01-21 15:16:21 +0200

    image generation fix

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,105 +1,117 @@
-import { NextRequest, NextResponse } from 'next/server';
-import sharp from 'sharp';
+import { NextRequest, NextResponse } from "next/server";
+import sharp from "sharp";
 import { Upload } from "@aws-sdk/lib-storage";
 import { S3Client } from "@aws-sdk/client-s3";
-import { Readable, Writable } from 'stream';
-import { getSession } from '../utils';
-import { ApiError, db, ErrorCode } from '@/utils/db';
-import { routeHandlers } from '@/utils/routeHandlers';
+import { Readable, Writable } from "stream";
+import { getSession } from "../utils";
+import { ApiError, db, ErrorCode } from "@/utils/db";
+import { routeHandlers } from "@/utils/routeHandlers";
 
 const s3 = new S3Client({
-    forcePathStyle: false,
-    endpoint: 'https://ams3.digitaloceanspaces.com',
-    region: 'ams3',
-    credentials: {
-        accessKeyId: process.env.DO_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.DO_SECRET_ACCESS_KEY!,
-    }
+  forcePathStyle: false,
+  endpoint: "https://ams3.digitaloceanspaces.com",
+  region: "ams3",
+  credentials: {
+    accessKeyId: process.env.DO_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.DO_SECRET_ACCESS_KEY!,
+  },
 });
 
 export const PUT = async (req: NextRequest) => {
-    if(!req.body)
-        return new NextResponse();
-    
-    console.log('got request data');
+  if (!req.body) return new NextResponse();
 
-    const newHeaders = {};
+  console.log("got request data");
 
-    const session = await getSession(newHeaders);
+  const newHeaders = {};
 
-    if(!session) {
-        const error = new ApiError(ErrorCode.UserSessionInvalid);
-        return NextResponse.json({error: error.message}, {status: error.statusCode});
-    }
-    
-    const content = await db.transaction().execute(async trx => {
-        const {id: contentId} = await routeHandlers.put.content({session, trx, headers: newHeaders, req});
-        return await routeHandlers.get.content({contentId, session, trx, headers: newHeaders, req});
+  const session = await getSession(newHeaders);
+
+  if (!session) {
+    const error = new ApiError(ErrorCode.UserSessionInvalid);
+    return NextResponse.json(
+      { error: error.message },
+      { status: error.statusCode }
+    );
+  }
+
+  const content = await db.transaction().execute(async (trx) => {
+    const { id: contentId } = await routeHandlers.put.content({
+      session,
+      trx,
+      headers: newHeaders,
+      req,
+    });
+    return await routeHandlers.get.content({
+      contentId,
+      session,
+      trx,
+      headers: newHeaders,
+      req,
+    });
+  });
+
+  if (!content) return NextResponse.json({});
+
+  const { name } = content;
+
+  const start = Date.now();
+
+  console.log("uploading");
+
+  const transform = sharp();
+
+  transform.setMaxListeners(0);
+
+  const createUpload = (transform: sharp.Sharp, name: string, ext: string) => {
+    const readable = Readable.toWeb(transform) as ReadableStream<any>;
+    const upload = new Upload({
+      client: s3,
+      params: {
+        Bucket: "menuet",
+        Key: `content/${name}.${ext}`,
+        ContentType: `image/${ext}`,
+        ACL: "public-read",
+        Body: readable,
+      },
     });
 
-    if(!content)
-        return NextResponse.json({});
-    
-    const {name} = content;
+    return upload.done();
+  };
 
-    const start = Date.now();
+  const createUploadClone = (width: number, height: number) => {
+    const clone = transform
+      .clone()
+      .resize({ height, width, fit: "cover", withoutEnlargement: true })
+      .webp({ quality: 80 });
 
-    console.log('uploading');
+    return createUpload(clone, `${name}-${width}-${height}`, "webp");
+  };
 
-    const transform = sharp();
-    
-    transform.setMaxListeners(0);
+  const createUploadSet = (size: number) => [
+    createUploadClone(size, size),
+    // createUploadClone(size, size/2),
+    // createUploadClone(size/2, size),
+  ];
 
-    const createUpload = (transform: sharp.Sharp, name: string, ext: string) => {
-        const readable = Readable.toWeb(transform) as ReadableStream<any>;
-        const upload = new Upload({
-            client: s3,
-            params: {
-                Bucket: 'menuet',
-                Key: `content/${name}.${ext}`,
-                ContentType: `image/${ext}`,
-                ACL: 'public-read',
-                Body: readable
-            }
-        });
+  const writable = Writable.toWeb(transform) as WritableStream<any>;
+  const readable = Readable.toWeb(transform) as ReadableStream<any>;
 
-        return upload.done();
-    }
+  const original = transform.clone().png({ quality: 100 });
 
-    const createUploadClone = (width: number, height: number) => {
-        const clone = transform
-            .clone()
-            .resize({height, width, fit: 'cover', withoutEnlargement: true})
-            .webp({quality: 80});
+  const uploads = [
+    createUpload(original, `${name}`, "png"),
+    ...createUploadSet(200),
+    ...createUploadSet(500),
+    ...createUploadSet(1000),
+  ];
 
-        return createUpload(clone, `${name}-${width}-${height}`, 'webp');
-    }
+  console.log("piping");
 
-    const createUploadSet = (size: number) => [
-        createUploadClone(size, size),
-        createUploadClone(size, size/2),
-        createUploadClone(size/2, size),
-    ];
+  req.body.pipeThrough({ writable, readable });
 
-    const writable = Writable.toWeb(transform) as WritableStream<any>;
-    const readable = Readable.toWeb(transform) as ReadableStream<any>;
+  await Promise.all(uploads);
 
-    const original = transform.clone().png({ quality: 100 });
+  console.log("upload done", Date.now() - start, "ms");
 
-    const uploads = [
-        createUpload(original, `${name}`, 'png'),
-        ...createUploadSet(200),
-        ...createUploadSet(500),
-        ...createUploadSet(1000),
-    ];
-
-    console.log('piping');
-
-    req.body.pipeThrough({writable, readable});
-
-    await Promise.all(uploads);
-
-    console.log('upload done', Date.now() - start, 'ms');
-    
-    return NextResponse.json(content);
+  return NextResponse.json(content);
 };
